aa2e030b8fb750d263c0efc686624755e52a859c
ilhan a coklar <iac7@afsaccess1.njit.edu>
2018-03-20 12:41:43 -0400

    First commit

---
+++ signup.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Login Page</title>
+  <style type="text/css">
+    div.container {
+      border: 10px solid #B3B6B7;
+      padding: 10px;
+      width: 400px;
+      margin:30px auto;
+      text-align:center;  
+    }
+    label {
+      text-align: left;
+    }
+    .login { 
+      background:#76D7C4;
+      text-align: center;
+      margin: auto;
+    }
+    .login div {
+      border:2px solid #76D7C4;
+      padding:3px;
+    }
+    .register { 
+      background:#76D7C4; 
+    }
+    .register div {
+      border:2px solid #76D7C4;
+      padding:3px;
+    }
+    input[type=text], input[type=number] {
+      padding: 6px 15px;
+      margin: 8px 0;
+      border: 1px solid #ccc;
+    }
+    button {
+      background-color: #FDFEFE;
+      color: #17202A;
+      font-family: Helvetica;
+      padding: 6px 15px;
+      margin: 8px 0;
+      width: 50%;
+    }
+  </style>
+</head>
+
+<body>
+  <div class='container' align="center">
+
+    <form class="login">
+
+      <div>
+        <label><b>First Name</b></label><br>
+        <input type="text" name="reg_fname" placeholder="Enter First name" required>
+      </div>
+
+        <div>
+        <label><b>Last Name</b></label>
+        <br> 
+        <input type="text" name="reg_lname" placeholder="Enter Last name" required>
+      </div>
+              
+        <div>
+        <label><b>Email</b></label><br>
+        <input type="text" name="reg_email" placeholder="Enter Email" required>
+      </div>
+      <div>
+
+      <div>
+        <label><b>Phone</b></label><br>
+        <input type="Number" name="reg_phone" placeholder="Enter Phone Number" required>
+      </div>
+      <div>
+
+        <div>
+        <label><b>Birthday</b></label><br>
+        <input type="Number" name="reg_bday" placeholder="Enter Birthday" required>
+      </div>
+      <div>
+
+        <div>
+        <label><b>Gender</b></label><br>
+        <input type="text" name="reg_gender" placeholder="Enter Gender" required>
+      </div>
+      <div>
+<br>
+        <button type="submit">Login</button>     
+      </div>
+    </form>
+
+    <form class="register">
+      <div>
+        <button type="submit">Sign up</button>
+      </div>
+    </form>
+
+  </div>
+</body>
+
+</html>
